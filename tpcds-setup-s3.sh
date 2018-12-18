2fbaeacccc5c269b776a761b07ea8dc94a120037
Michael Young <michael.l.young@gmail.com>
2018-12-18 17:05:39 -0500

    Update script to support new sql files for S3.

--- tpcds-setup-s3.sh
+++ tpcds-setup-s3.sh
@@ -50,11 +50,54 @@ if [ $SCALE -eq 1 ]; then
 	exit 1
 fi
 
-# Create the text/flat tables as external tables. These will be later be converted to ORCFile.
-echo "Loading text data into external tables."
-runcommand "hive -i settings/load-flat.sql -f ddl-tpcds/text/alltables-s3.sql -d DB=tpcds_bin_partitioned_orc_${SCALE} -d LOCATION=${DIR}"
-echo "Data loaded into database ${DB}."
+# Tables in the TPC-DS schema.
+DIMS="date_dim time_dim item customer customer_demographics household_demographics customer_address store promotion warehouse ship_mode reason income_band call_center web_page catalog_page web_site"
+FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_returns inventory"
 
-echo "Analyzing tables."
-runcommand "hive -i settings/load-flat.sql -f ddl-tpcds/text/analyze_everything.sql -d DB=tpcds_bin_partitioned_orc_${SCALE}"
-echo "Tables analyzed."
+# Create the partitioned and bucketed tables.
+if [ "X$FORMAT" = "X" ]; then
+	FORMAT=orc
+fi
+
+LOAD_FILE="load_${FORMAT}_${SCALE}.mk"
+SILENCE="2> /dev/null 1> /dev/null" 
+if [ "X$DEBUG_SCRIPT" != "X" ]; then
+	SILENCE=""
+fi
+
+echo -e "all: ${DIMS} ${FACTS}" > $LOAD_FILE
+
+i=1
+total=24
+DATABASE=tpcds_bin_partitioned_${FORMAT}_${SCALE}
+MAX_REDUCERS=2500 # maximum number of useful reducers for any scale 
+REDUCERS=$((test ${SCALE} -gt ${MAX_REDUCERS} && echo ${MAX_REDUCERS}) || echo ${SCALE})
+
+# Populate the smaller tables.
+for t in ${DIMS}
+do
+	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/s3/${t}.sql \
+	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
+        -d SCALE=${SCALE} \
+	    -d REDUCERS=${REDUCERS} \
+		-d LOCATION=${DIR}"
+	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
+	i=`expr $i + 1`
+done
+
+for t in ${FACTS}
+do
+	COMMAND="hive -i settings/load-partitioned.sql -f ddl-tpcds/s3/${t}.sql \
+	    -d DB=tpcds_bin_partitioned_${FORMAT}_${SCALE} \
+        -d SCALE=${SCALE} \
+	    -d BUCKETS=${BUCKETS} \
+	    -d RETURN_BUCKETS=${RETURN_BUCKETS} \
+		-d REDUCERS=${REDUCERS} \
+		-d LOCATION=${DIR}"
+	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
+	i=`expr $i + 1`
+done
+
+make -j 2 -f $LOAD_FILE
+
+echo "Data loaded into database ${DATABASE}."
